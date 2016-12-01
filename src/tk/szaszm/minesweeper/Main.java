0acfd16276d47f8b2dc49a10da661002dcd9890b
Szász Márton <szaszm01@gmail.com>
2016-12-01 03:49:33 +0100

    documentation, final changes

--- src/tk/szaszm/minesweeper/Main.java
+++ src/tk/szaszm/minesweeper/Main.java
@@ -3,6 +3,11 @@ package tk.szaszm.minesweeper;
 import java.io.IOException;
 
 public class Main {
+    /**
+     * Creates an Application and runs it.
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
         Application app = new Application();
         app.run();
